e49e0cd2fdad33ee6332f2daf7981607af975f6b
Furkan Özgel <furkan.ozgel@hotmail.com>
2024-10-30 01:14:51 +0300

    Refactor TaskInput bottomButtons MydayBtn component to fix background color transparency issue

--- src/screens/Todo/TodoList/components/TaskInput/bottomButtons/MydayBtn/MydayBtn.tsx
+++ src/screens/Todo/TodoList/components/TaskInput/bottomButtons/MydayBtn/MydayBtn.tsx
@@ -9,7 +9,7 @@ import { SvgXml } from 'react-native-svg';
 function MydayBtn({ButtonSvg, Title, onPress, makeToday}: {ButtonSvg: string, Title: string, onPress: () => void, makeToday: any}): React.JSX.Element {
     return(
         <TouchableOpacity onPress={onPress}>
-            <View style={[styles.down_btn, {backgroundColor: makeToday ? "blue": "rgb(0,0,0,0)"}]}>
+            <View style={[styles.down_btn, {backgroundColor: makeToday ? "blue": "rgba(0,0,0,0)"}]}>
                 <SvgXml xml={ButtonSvg} width="24" height="24" fill={theme.text.color} />
                 <Text style={styles.down_btn_text}>{Title}</Text>
             </View>
