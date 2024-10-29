8f9f98f61eec506ea033a7035faec89aaf60a7b0
Furkan Özgel <furkan.ozgel@hotmail.com>
2024-10-30 00:47:18 +0300

    Refactor TaskInput component to remove unused PopupTemplate style file and update PopupTemplate component to include a cancel area and fix container height in Repeat modal

--- src/screens/Todo/TodoList/components/TaskInput/btn_modals/PopupTemplate/PopupTemplate.tsx
+++ src/screens/Todo/TodoList/components/TaskInput/btn_modals/PopupTemplate/PopupTemplate.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import {
-    View,
-    Text,
-} from 'react-native';
-
-import styles from './PopupTemplate.style';
+import React, {useState} from 'react';
+import { View, Dimensions, TouchableWithoutFeedback, StyleSheet } from 'react-native';
 
 interface PopupTemplateProps {
     children: React.ReactNode;
     visible: boolean;
     setVisible: (visible: string) => void;
+    containerHeight: number;
 }
 
-function PopupTemplate({children ,visible, setVisible}: PopupTemplateProps): React.JSX.Element {
+function PopupTemplate({children ,visible, setVisible, containerHeight}: PopupTemplateProps): React.JSX.Element {
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+    
+    const styles = StyleSheet.create({
+        cancelArea: {
+            height: Dimensions.get("window").height, 
+            width:"100%", 
+            position: "absolute", 
+            top:-(Dimensions.get("window").height - containerHeight)
+        }
+    });
+    
+    if (!visible) {
+        return <></>;
+    }
+
     return(
         <View>
-            <Text>PopupTemplate</Text>
-            {visible && children}
+            <TouchableWithoutFeedback onPress={() => setVisible('')}>
+                <View style={styles.cancelArea}/>
+            </TouchableWithoutFeedback>
+            {children}
         </View>
     );
 };
--- src/screens/Todo/TodoList/components/TaskInput/btn_modals/Repeat/Repeat.tsx
+++ src/screens/Todo/TodoList/components/TaskInput/btn_modals/Repeat/Repeat.tsx
@@ -15,9 +15,16 @@ interface RepeatProps {
 
 function Repeat({visible, setVisible}: RepeatProps): React.JSX.Element {
     return(
-        <PopupTemplate visible={visible} setVisible={setVisible}>
-            <View style={{}}>
-                <Text style={{}}>Repeat</Text>
+        <PopupTemplate visible={visible} setVisible={setVisible} containerHeight={100}>
+            <View style={{backgroundColor:"blue", width: 40, height: 100}}>
+                <Text style={{}}>1</Text>
+                <Text style={{}}>2</Text>
+                <Text style={{}}>3</Text>
+                <Text style={{}}>4</Text>
+                <Text style={{}}>5</Text>
+                <Text style={{}}>6</Text>
+                <Text style={{}}>7</Text>
+                <Text style={{}}>8</Text>
             </View>
         </PopupTemplate>
     );
