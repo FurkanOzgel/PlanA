28619609d10dba5ddce9f6ac1ab71377e1c90258
Furkan Özgel <furkan.ozgel@hotmail.com>
2024-11-03 01:32:23 +0300

    Rename Android.js to Android.ts and update import statement for TypeScript compatibility

--- src/utils/Android.ts
+++ src/utils/Android.ts
@@ -1,4 +1,4 @@
-import { PermissionsAndroid } from "react-native"
+import { PermissionsAndroid, Platform } from "react-native";
 
 const checkNotificationPermission = async () => {
     if (Platform.OS === 'android') {
